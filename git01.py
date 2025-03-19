858a48aaab09c6f2d0f2b464eac2da2793e7543c
dgsw0951 <dgsw0951@dgsw0951ui-MacBookPro.local>
2025-03-19 20:29:27 +0900

    add if

--- git01.py
+++ git01.py
@@ -2,3 +2,7 @@ print("Hi, What's your name?")
 name=input("My name is ")
 print(f"{name}, which do you want to do, reading or sleeping?")
 do=input("")
+if do=="reading":
+    print("OK, let's read together!")
+else:
+    print("OK, let's sleeping together")
