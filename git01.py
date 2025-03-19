2ca6de38f387569fd1322ff848d61ae0a97f4c3d
dgsw0951 <dgsw0951@dgsw0951ui-MacBookPro.local>
2025-03-19 20:32:13 +0900

    add script

--- git01.py
+++ git01.py
@@ -1,8 +1,12 @@
 print("Hi, What's your name?")
 name=input("My name is ")
 print(f"{name}, which do you want to do, reading or sleeping?")
-do=input("")
+do=input("reading/sleeping")
 if do=="reading":
     print("OK, let's read together!")
+    print("What will we read?")
+    book=input("book name : ")
 else:
     print("OK, let's sleeping together")
+    print("What about play a song?")
+    a1=input("Y/N")
