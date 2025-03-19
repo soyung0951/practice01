f4599fb26ce4185140a2f1e57559fcf95d285dc2
dgsw0951 <dgsw0951@dgsw0951ui-MacBookPro.local>
2025-03-19 20:39:37 +0900

    add ending 2

--- git01.py
+++ git01.py
@@ -6,7 +6,7 @@ if do=="reading":
     print("OK, let's read together!")
     print("What will we read?")
     book=input("book name : ")
-    print(f"Ok, then have a good time together with {book}. :)")
+    print(f"Thanks, then have a good time together with {book}. :)")
 else:
     print("OK, let's sleeping together")
     print("What about play a song?")
@@ -14,5 +14,6 @@ else:
     if a1=="Y":
         print("Please tell me the song's title which you want to listen to?")
         song=input("")
+        print(f"Thanks, then have a good night with {song}. :)")
     else:
         print("OK, then good night :)")
