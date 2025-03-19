59b841108e5c3e4fc1f7897a9318619622f3e96e
dgsw0951 <dgsw0951@dgsw0951ui-MacBookPro.local>
2025-03-19 20:27:03 +0900

    .

--- git.py
+++ git.py
@@ -1,1 +1,3 @@
-print("Hello World")
+name=input("My name is ")
+print(f"Hello, {name}! Nice to meet you!")
+print("Which do you want to do?")
---
+++ git01.py
@@ -0,0 +1,4 @@
+print("Hi, What's your name?")
+name=input("My name is ")
+print(f"{name}, which do you want to do, reading or sleeping?")
+do=input("")
